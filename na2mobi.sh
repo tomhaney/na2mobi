87b62fe5968e36e95d46fd666937981f6aa3ed69
Tom Haney <tom@lavasculpt.com>
2014-12-05 09:21:14 -0500

    * Added check for 404 error on now.nashownotes.com * Added the ability to download past episodes with the episode number as a parameter.

--- na2mobi.sh
+++ na2mobi.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 # script depends on lynx, pandoc, and calibre
+echo "usage: na2mobi.sh with no parameters will download the latest show notes. Use an episode number to download past show notes, e.g. ./na2mobi.sh 667"
 set -x #echo on
+if [[ $1 ]]; then
+    url=http://$1.nashownotes.com
+else 
+    url=http://now.nashownotes.com
+fi
+echo $url
+
 # download the show notes as a text file
-lynx -dump http://now.nashownotes.com > na-now-pre.txt
-# remove non-printable characters (e.g episode 658) on advice from http://alvinalexander.com/blog/post/linux-unix/how-remove-non-printable-ascii-characters-file-unix
+lynx -dump $url > na-now-pre.txt
+# remove non-printable characters (e.g episode 658) 
 tr -cd '\11\12\15\40-\176' < na-now-pre.txt > na-now.txt
 # prevent seperation lines from being converted to headlines later
 sed -i 's/          + --------------------------------------------------------------//' na-now.txt
@@ -21,6 +29,22 @@ sed -i 's/                 //' na-now.txt
 sed -i ':a;N;$!ba;s/\n   /  \n\t/g' na-now.txt
 # find the first mention of a jpg or png and remove everthing before the last space on that line
 sed -i 's/               o/   /' na-now.txt
+file=na-now.txt
+minimumsize=1000
+actualsize=$(wc -c "$file" | cut -f 1 -d ' ')
+if [ $actualsize -ge $minimumsize ]; then
+    echo size is over $minimumsize bytes
+else
+    echo size is under $minimumsize bytes
+    cat na-now.txt
+set +x
+    echo "
+Try again in a few hours.  A new episode could be uploading now.
+
+"
+set -x
+    exit
+fi
 ART=$(grep art na-now.txt | grep -E -m 1 'jpg]|png]' | sed 's/.* \[//')
 # Remove trailing white space from file name
 ART="${ART%"${ART##*[![:space:]]}"}"
